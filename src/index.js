defaa2e841dc43df1f0b1b49f1dc0b2ed16431c8
Dani Setien <dani.setien@gmail.com>
2023-01-17 23:30:43 +0100

    Refactor console.table in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { strictEquals } from "./strictEquals/strictEquals.js";
-import { tests, testsExpectedResults } from "./tests/tests.js";
+import { tests, testsExpectedResults } from "./tests/index.js";
 
 /* tests.map((test) => console.log(strictEquals(test))); */
 
 console.table(
-  tests.flatMap((x, i) => [
-    `strictEquals returns ${strictEquals(x)}, and the expected result is ${
-      testsExpectedResults[i]
+  tests.flatMap((test, result) => [
+    `strictEquals returns ${strictEquals(test)}, and the expected result is ${
+      testsExpectedResults[result]
     }`,
   ])
 );
